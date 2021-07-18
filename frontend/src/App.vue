2b4ae4d268f4cc3a96cc6122340e2f2cca44b48e
josevenzke <josevenzkejr@gmail.com>
2021-07-18 18:30:30 -0300

    added new component and removed old one

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@
             <!-- Menu Nav -->
             <div class="window" style="width: 100%">
               <div class="title-bar">
-                <div class="title-bar-text"><a href="index.html"><button disabled>Sobre</button></a> <a href="project.html"><button>Projetos</button></a> <a href="contato.html"><button>Contato</button></a></div>
+                <div class="title-bar-text"></div>
                 <div class="title-bar-controls">
                   <button aria-label="Minimize" id="minimize"></button>
                   <button aria-label="Maximize" id="maximize"></button>
@@ -19,35 +19,7 @@
             </div>
             <!-- Fim Botoes Windows  -->
           </div>
-          <!-- Fim Barra de Cima -->
-
-          <!-- Seção Esquerda -->
-          <div class="left-column bottom" >
-            <img src="images/logo.jpg" id="profile_pic" alt="My photo">
-            <h5 class="name-title">José Venzke</h5>
-            <p id="settings-note">Fullstack Software Developer</p>
-            <div id="theme-options-wrapper">
-              <div class="ico"><i class="fab sub-icon fa-linkedin"></i></div>
-              <div class="ico"><i class="fab sub-icon fa-github-square"></i></div>
-            </div>
-            
-          </div>
-          <!-- Fim Seção Esquerda -->
-
-          <!-- Seção Direita -->
-          <div class="right-column bottom">
-            <div class="window">
-              <div class="title-bar">
-                <div class="title-bar-text" id="titulo">About Me</div>
-                <div class="title-bar-controls">
-                </div>
-              </div>
-              <div class="window-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,sse</p>
-              </div>
-            </div>
-          </div>
-          <!-- Fim Seção Direita -->
+            <About />
           </div>
       <!-- Fim Principal -->
       </div>
@@ -64,8 +36,10 @@
 </template>
 
 <script>
+import About from './components/About.vue'
 
 export default {
+  components: { About },
   name: 'App',
   metaInfo:{
     title: 'Default Title',
