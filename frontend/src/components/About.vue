180e942ad93890ab36b71536d85e80614be13f7d
josevenzke <josevenzkejr@gmail.com>
2021-07-18 18:29:29 -0300

    add about section component

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,29 @@
+<template>
+    
+        <div class="left-column bottom" >
+            <img src="images/logo.jpg" id="profile_pic" alt="My photo">
+            <h5 class="name-title">José Venzke</h5>
+            <p id="settings-note">Fullstack Software Developer</p>
+            <div id="theme-options-wrapper">
+              <div class="ico"><i class="fab sub-icon fa-linkedin"></i></div>
+              <div class="ico"><i class="fab sub-icon fa-github-square"></i></div>
+            </div>
+            
+          </div>
+          <!-- Fim Seção Esquerda -->
+
+          <!-- Seção Direita -->
+          <div class="right-column bottom">
+            <div class="window">
+              <div class="title-bar">
+                <div class="title-bar-text" id="titulo">About Me</div>
+                <div class="title-bar-controls">
+                </div>
+              </div>
+              <div class="window-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,sse</p>
+              </div>
+            </div>
+          </div>
+
+</template>
