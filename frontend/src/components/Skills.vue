ba8a61872c39b366dff9f413178200d3cf0e05db
josevenzke <josevenzkejr@gmail.com>
2021-07-21 19:47:03 -0300

    component for skills section

---
+++ frontend/src/components/Skills.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="de-flex">
+    <div class="left-column">
+        <ul class="tree-view">
+            <li>Table of Contents</li>
+            <li>What is web development?</li>
+            <li>
+                CSS
+                <ul>
+                <li>Selectors</li>
+                <li>Specificity</li>
+                <li>Properties</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="right-column">
+        <ul class="tree-view">
+            <li>
+                <details open>
+                <summary>JavaScript</summary>
+                <ul>
+                    <li>Avoid at all costs</li>
+                    <li>
+                    <details>
+                        <summary>Unless</summary>
+                        <ul>
+                        <li>Avoid</li>
+                        <li>
+                            <details>
+                            <summary>At</summary>
+                            <ul>
+                                <li>Avoid</li>
+                                <li>At</li>
+                                <li>All</li>
+                                <li>Cost</li>
+                            </ul>
+                            </details>
+                        </li>
+                        <li>All</li>
+                        <li>Cost</li>
+                        </ul>
+                    </details>
+                    </li>
+                </ul>
+                </details>
+            </li>
+            <li>HTML</li>
+        </ul>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.de-flex{
+    display: flex;
+    justify-content: space-around;
+}
+
+ul{
+    text-align: left;
+}
+
+</style>
