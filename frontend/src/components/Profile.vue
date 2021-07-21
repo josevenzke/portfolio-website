b17bbf1ecb840282be8a437319d349ef084fe175
josevenzke <josevenzkejr@gmail.com>
2021-07-21 19:46:40 -0300

    component for profile section

---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="left-column bottom" >
+        <img src="images/logo.jpg" id="profile_pic" alt="My photo">
+        <h5 class="name-title">José Venzke</h5>
+        <p id="settings-note">Fullstack Software Developer</p>
+        <div id="theme-options-wrapper">
+            <div class="ico"><i class="fab sub-icon fa-linkedin"></i></div>
+            <div class="ico"><i class="fab sub-icon fa-github-square"></i></div>
+        </div>
+            
+    </div>
+</template>
