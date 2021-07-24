64e42953f69c20cce4f55c5f974607e3dc9e1c69
josevenzke <josevenzkejr@gmail.com>
2021-07-23 23:44:02 -0300

    new tab englobing About and Profile components

---
+++ frontend/src/components/AboutTab.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="about-container">
+        <div class="left-column">
+            <Profile />
+        </div>
+        <div class="right-column">
+            <About />
+        </div>
+    </div>
+</template>
+
+<script>
+import Profile from './Profile.vue'
+import About from './About.vue'
+
+export default {
+    name:'AboutTab',
+    components:{
+        Profile,
+        About
+    }
+}
+</script>
+
+<style scoped>
+
+.about-container{
+  display: flex;
+  justify-content: space-around;
+}
+.right-column{
+  margin:50px 20px 40px 20px;
+}
+
+.left-column{
+    margin: 50px 20px 20px 20px;
+}
+
+
+@media only screen and (max-width: 1250px) {
+
+}
+
+@media only screen and (max-width: 880px) {
+
+  .about-container {
+    flex-direction: column;
+    width: 100%;
+
+  }
+}
+
+</style>
