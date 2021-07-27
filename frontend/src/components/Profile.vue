70354cfca57b45a616ba4ca5aba6a38de512958c
josevenzke <josevenzkejr@gmail.com>
2021-07-26 21:20:38 -0300

    removed accent

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <img src="images/logo.jpg" id="profile_pic" alt="My photo">
-        <h5 class="name-title">José Venzke</h5>
+        <h5 class="name-title">Jose Venzke</h5>
         <p id="settings-note">Fullstack Software Developer</p>
         <div id="theme-options-wrapper">
             <div class="ico"><i class="fab sub-icon fa-linkedin"></i></div>
