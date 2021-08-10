53e1534e27382a532c760116eff3d237958b7bb1
josevenzke <josevenzkejr@gmail.com>
2021-08-10 00:28:18 -0300

    now shows clippy with a helping image

--- frontend/src/components/TextHelper.vue
+++ frontend/src/components/TextHelper.vue
@@ -1,13 +1,61 @@
 <template>
-        <div>
-            
-        </div>
+  <transition name="fade">
+    <div v-if="showText" class="clippy">
+      <p class="clippy-text">
+        Você pode abrir o Portfolio<br />
+        clicando no icone a esquerda
+      </p>
+      <img src="../../public/images/teste.png" alt="" />
+    </div>
+  </transition>
 </template>
 
 <script>
-   
+export default {
+  name: "TextHelper",
+  data() {
+    return {                            
+        showText: false, 
+    };
+  },
+  created(){
+      this.textHelp()
+  },
+  methods: {
+    async textHelp() {
+        await this.sleep(500)
+        this.showText = true
+        await this.sleep(4000)
+        this.showText = false
+        },
+    sleep(ms){
+        return new Promise(resolve => setTimeout(resolve, ms));
+        },
+    }
+}
 </script>
 
 <style scoped>
+.clippy {
+  position: relative;
+  width: 207px;
+  margin-top: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.clippy-text {
+  position: absolute;
+  top: 3px;
+  left: 25px;
+}
 
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
